db204c7b94e426fab493ce4b642a358939aca4d3
Antoniobellom <61624925+Antoniobellom@users.noreply.github.com>
2021-12-28 18:03:53 -0300

    first

--- script.js
+++ script.js
@@ -1,42 +1,119 @@
 
 window.onload = () => {
+    let cardNumbers = {
+        number:" ",
+        icon:" ",
+    };
+    console.log(cardNumbers);
 
-    
-    
-    
+    let cardSort = (cardNumbers) => {
+        for (let i = 0; i < cardNumbers.length; i++) {
+            for (let j = 0; j < cardNumbers.length; j++) {
+                if (cardNumbers[j] > cardNumbers[j + 1]) {
+                    let temp = cardNumbers[j];
+                    cardNumbers[j] = cardNumbers[j + 1];
+                    cardNumbers[j + 1] = temp;
+                }
+            }
+        }
+        return cardNumbers;
+    }
+    function getCardType(cardNum) {
+        return cardNum === 11
+          ? "J"
+          : cardNum === 12
+          ? "Q"
+          : cardNum === 13
+          ? "K"
+          : cardNum === 1
+          ? "A"
+          : cardNum;
+      }
+     
+
+        
     let generateNumber = () => {
-        let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+        let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
         let indexNum = Math.floor(Math.random() * numeros.length);
         return numeros[indexNum];
     }
     let generateIcon = () => {
-        
         let iconos = ["\u2665", "\u2666", "\u2660", "\u2663"];
         let indexIcon = Math.floor(Math.random() * iconos.length);
-        let colorIcono = document.querySelector(".icono")
-        let colorIcono2 = document.querySelector(".icono2")
-        if(iconos[indexIcon] == "\u2665" || iconos[indexIcon] == "\u2666")
+        return iconos[indexIcon];
+    }
+
+    let generateCard = (cardNum) =>{
+
+        const icon = generateIcon();
+
+        if(icon == "\u2665" || icon == "\u2666")
         {
-           colorIcono.classList.replace("text-dark","text-danger")
-           colorIcono2.classList.replace("text-dark","text-danger")
+            $('#cardContainer').append(
+                '<div class="container-fluid text-center card">' +
+                '<div class="icono text-danger">' +
+                icon +
+                '</div>' +
+                '<div id="medio">' +
+                getCardType(cardNum) +
+                '</div>' +
+                '<div class="icono2 text-danger">' +
+                icon +
+                '</div>' +
+                '</div>');
         }
         else{
-            colorIcono.classList.replace("text-danger","text-dark")
-            colorIcono2.classList.replace("text-danger","text-dark")
+            $('#cardContainer').append(
+                '<div class="container-fluid text-center card">' +
+                '<div class="icono text-dark ">' +
+                icon +
+                '</div>' +
+                '<div id="medio">' +
+                getCardType(cardNum) +
+                '</div>' +
+                '<div class="icono2 text-dark">' +
+                icon +
+                '</div>' +
+                '</div>');
         }
-        return iconos[indexIcon];
-        
         
     }
-    let icono = generateIcon()
-    document.querySelector('.icono').innerHTML = icono;
-    document.querySelector('.icono2').innerHTML = icono;
-    document.getElementById("medio").innerHTML = generateNumber();
+    
     document.querySelector(".boton").addEventListener("click",()=>{
-    document.getElementById("medio").innerHTML = generateNumber();
-    let icono2 = generateIcon()
-    document.querySelector('.icono').innerHTML = icono2;
-    document.querySelector('.icono2').innerHTML = icono2;
+        $('#cardContainer').empty();
+        cardNumbers = [];
 
+        const amnt = $("#cardAmmount").val();
+        let i = 0;
+        
+
+        while (i < amnt) {
+            cardNumbers[i]=generateNumber()
+                
+            
+            i++;
+        }
+        i = 0;
+        while (i < amnt) {
+            generateCard(cardNumbers[i]);
+            i++;
+        }
     })
+
+    document.querySelector("#sortingBtn").addEventListener("click",()=>{
+        $('#cardContainer').empty();
+
+        cardNumbers = cardSort(cardNumbers);
+        
+        let i = 0;
+        while (i < cardNumbers.length) {
+            generateCard(cardNumbers[i]);
+            i++;
+        }
+    })
+    document.querySelector("#clearBtn").addEventListener("click",()=>{
+        $('#cardContainer').empty();
+        cardNumbers = [];
+    })
+    
 }
